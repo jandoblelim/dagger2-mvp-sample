1efe3621d3c3ffa2d68ec94a21fef2a67dbe378d
Jan Lim <jan@ninjavan.sg>
2016-11-03 11:23:13 +0800

    initial commit.

---
+++ dagger2-mvp-sample/dagger2-mvp-sample/app/src/main/java/devskywalker/dagger2_mvp_sample/MainActivity.java
@@ -0,0 +1,13 @@
+package devskywalker.dagger2_mvp_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
